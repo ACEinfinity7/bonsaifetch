43683fcb7345f8767663e00233a9d5d0d1302926
Alex Elko <acelko@Alexs-MacBook-Pro.local>
2022-02-20 12:25:56 -0500

    updated path to reflect hidden directory

--- neo_bonsai.sh
+++ neo_bonsai.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-~/startup/bonsai.sh -n -L 20 -g 35,20 > .my_bonsai_art.txt
+~/.startup/bonsai.sh -n -L 20 -g 35,20 > .my_bonsai_art.txt
 neofetch --disable packages gpu memory resolution --ascii .my_bonsai_art.txt --ascii_colors 11 3 10 2 0
 
